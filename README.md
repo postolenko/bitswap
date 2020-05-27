42af7571781e57160149231c3f5a1cc071801768
postolenko <denis.postolenko@gmail.com>
2020-05-27 22:28:16 +0300

    Bitswap

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/bitswap/index.html
---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+  // $(".article .right > ul > li").each(function() {
+  //   var indeItem = $(this).index("li");
+  //   console.log(indeItem);
+  //   if(indeItem % 5) {
+  //     $(this).addClass("five");
+  //   }
+  // });
+
+});
