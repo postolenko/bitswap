2f7b3cf1c46f3ce75083707450393a62060bce88
postolenko <denis.postolenko@gmail.com>
2020-05-29 05:56:58 +0300

    BitSwap

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+function getAnimation() {
+  $(".animate").each(function() {
+    if( $(this).offset().top <= $(document).scrollTop() + $(window).height() ) {
+      $(this).addClass("active");
+    }
+  });
+}
+
 var w = window,
 d = document,
 e = d.documentElement,
@@ -7,23 +15,19 @@ bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
 $(window).load(function() {
 
-
-
 });
 
 $(window).resize(function() {
-
-
-
+	getAnimation();
 });
 
 $(document).scroll(function() {
-
-
-
+	getAnimation();
 });
 
 $(document).ready(function() {
+
+	getAnimation();
 
 	$('.article .right > ul > li:nth-child(5n+5)').addClass('five');
 	$('.sidebar > ul > li:nth-child(5n+5)').addClass('five');
